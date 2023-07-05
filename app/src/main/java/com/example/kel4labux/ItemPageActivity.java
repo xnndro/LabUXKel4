36d8b62063ddfa990de9250a6ea041a4ba11095f
Alexandro Alvin Valentino <alexandro.alvin98@gmail.com>
2023-07-06 00:07:30 +0700

    itemPage

---
+++ app/src/main/java/com/example/kel4labux/ItemPageActivity.java
@@ -0,0 +1,17 @@
+package com.example.kel4labux;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class ItemPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_page);
+
+
+    }
+}
