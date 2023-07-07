7261d8c8087d04b6c064b842519f97695ca3385a
Alexandro Alvin Valentino <alexandro.alvin98@gmail.com>
2023-07-07 10:51:37 +0700

    add detailpage from itempage

---
+++ app/src/main/java/com/example/kel4labux/DetailItem.java
@@ -0,0 +1,29 @@
+package com.example.kel4labux;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DetailItem extends AppCompatActivity {
+    private Item item;
+    TextView itemName, itemPrice, storeName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_item);
+
+        if(getIntent().hasExtra("item")){
+            item = getIntent().getParcelableExtra("item");
+        }
+
+//        assign to view
+        itemName = findViewById(R.id.tv_itemName);
+        itemPrice = findViewById(R.id.tv_itemPrice);
+        storeName = findViewById(R.id.tv_storeName);
+
+        itemName.setText(item.getName());
+        itemPrice.setText(item.getPrice());
+        storeName.setText(item.getStoreName());
+    }
+}
